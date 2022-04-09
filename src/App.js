103a43aaece9b34b32aaa89cd39ca0f5d71ad2ac
nurulamin092 <mnurulamin092@gmial.com>
2022-04-09 22:07:11 +0600

    update profile

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap';
 import { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import app from './firebase.init';
 
 const auth = getAuth(app);
 
 function App() {
   const [validated, setValidated] = useState(false);
+  const [name, setName] = useState('')
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [registered, setRegistered] = useState(false)
+  const [error, setError] = useState('')
+  const handleNameBluer = (event) => {
+    setName(event.target.value);
+  }
   const handleEmailBluer = (event) => {
     setEmail(event.target.value);
   }
@@ -63,6 +68,7 @@ function App() {
           const user = result.user;
           console.log(user);
           emailVerification();
+          handleUpdateProfile();
         })
         .catch((error) => {
           const errorMessage = error.message;
@@ -70,11 +76,27 @@ function App() {
         })
     }
   }
+  const handleUpdateProfile = () => {
+    updateProfile(auth.currentUser, {
+      displayName: name
+    }).then(() => {
+      console.log('update profile');
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
   return (
     <div>
       <div className='registered w-50 mx-auto my-5'>
         <Form validated={validated} onSubmit={handleRegisterForm}>
           <h1>Please {registered ? 'Login' : 'Register'}!!</h1>
+          {!registered && <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control onBlur={handleNameBluer} type="text" placeholder="Your Name" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>}
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control onBlur={handleEmailBluer} type="email" placeholder="Enter email" required />
